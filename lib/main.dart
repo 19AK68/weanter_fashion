48351ef1fe6b2dd11ca0880ad2476680e74857d1
Andrey Kushnaryov <akushnaryov@rightandabove.com>
2021-08-09 14:58:12 +0300

    refacroting custom drawer

--- lib/main.dart
+++ lib/main.dart
@@ -39,60 +39,10 @@ class WeatherFashionApp extends StatelessWidget {
         ),
 
       ),
-      drawer: Custom_Drawer(),
+      drawer: CustomDrawer(),
 
     );
   }
 }
 
-class Custom_Drawer extends StatelessWidget {
-  const Custom_Drawer({
-    Key? key,
-  }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return
-      SizedBox(
-        width: MediaQuery.of(context).size.width * 0.75,//20.0,
-        child: Drawer(
-          child: ListView(
-          children: <Widget>[
-            new DrawerHeader(
-              // margin: EdgeInsets.zero,
-              padding: EdgeInsets.zero,
-              child: UserAccountsDrawerHeader(
-                  decoration: BoxDecoration(color: Colors.green[200]),
-                  accountName: Text('Irina Last'),
-                  accountEmail: Text("WF@weatheranfasion.com"),
-                  currentAccountPicture: new CircleAvatar(
-                    radius:60,
-
-
-
-                    backgroundColor: Colors.blue[100],
-                    backgroundImage:
-                    Image.asset("assets/user_irina.jpg",fit: BoxFit.fill, width: 60,height: 60,
-                    ).image, // for Network image
-                  )),
-            ),
-            new ListTile(
-                title: new Text("About"),
-                leading: Icon(Icons.account_box),
-                onTap: (){
-                  Navigator.pop(context);
-                }
-            ),
-            new ListTile(
-                title: new Text("Setting"),
-                leading: Icon(Icons.settings),
-                onTap: (){
-                  Navigator.pop(context);
-                }
-            )
-          ],
-    ),
-        ),
-      );
-  }
-}
--- lib/ui/widgets/custom_drawer.dart
+++ lib/ui/widgets/custom_drawer.dart
@@ -1,37 +1,59 @@
 import 'package:flutter/material.dart';
-
 class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({
+    Key? key,
+  }) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return
-      ListView(
-      children: <Widget>[
-        new DrawerHeader(
-        // margin: EdgeInsets.zero,
-          padding: EdgeInsets.zero,
-          child: UserAccountsDrawerHeader(
-              decoration: BoxDecoration(color: Colors.blue[300]),
-              accountName: Text('Irina Last'),
-              accountEmail: Text("WF@weatheranfasion.com"),
-              currentAccountPicture: new CircleAvatar(
-                radius:60,
-                backgroundColor: Colors.blue[100],
-                backgroundImage:
-                Image.asset("assets/user_irina.jpg",fit: BoxFit.fill, width: 60,height: 60,
-                ).image, // for Network image
-              )),
+      SizedBox(
+        width: MediaQuery.of(context).size.width * 0.75,//20.0,
+        child: Drawer(
+          child: ListView(
+            children: <Widget>[
+              new DrawerHeader(
+                // margin: EdgeInsets.zero,
+                padding: EdgeInsets.zero,
+                child: UserAccountsDrawerHeader(
+                    decoration: BoxDecoration(color: Colors.green[200]),
+                    accountName: Text('Irina Last'),
+                    accountEmail: Text("WF@weatheranfasion.com"),
+                    currentAccountPicture: new CircleAvatar(
+                      radius:60,
+
+
+
+                      backgroundColor: Colors.blue[100],
+                      backgroundImage:
+                      Image.asset("assets/user_irina.jpg",fit: BoxFit.fill, width: 60,height: 60,
+                      ).image, // for Network image
+                    )),
+              ),
+              new ListTile(
+                  title: new Text("About"),
+                  leading: Icon(Icons.account_box),
+                  onTap: (){
+                    Navigator.pop(context);
+                  }
+              ),
+              new ListTile(
+                  title: new Text("Setting"),
+                  leading: Icon(Icons.settings),
+                  onTap: (){
+                    Navigator.pop(context);
+                  }
+              ),
+              new ListTile(
+                  title: new Text("Tema"),
+
+                  onTap: (){
+                    Navigator.pop(context);
+                  }
+              )
+            ],
+          ),
         ),
-        new ListTile(
-            title: new Text("О себе"),
-            leading: Icon(Icons.account_box),
-            onTap: (){}
-        ),
-        new ListTile(
-            title: new Text("Настройки"),
-            leading: Icon(Icons.settings),
-            onTap: (){}
-        )
-      ],
-    );
+      );
   }
 }
